2a8690152970250739820e74665213d7f78b7103
HIRAPARANAYAN <hiraparanayan98@gmail.com>
2023-06-07 14:20:45 +0530

    first commit

---
+++ views.py
@@ -0,0 +1,29 @@
+from django.http import HttpResponse
+from django.shortcuts import render,redirect
+
+def index(request):
+    return render(request,'index.html',{'navname':'index'})
+
+def company(request):
+    return render(request, 'company.html',{'navname':'company'})
+
+def contactus(request):
+    return render(request, 'contactus.html',{'navname':'contactus'})
+
+def marvel(request):
+    return render(request, 'marvel.html',{'navname':'marvel'})
+
+def universal(request):
+    return render(request, 'universal.html',{'navname':'universal'})
+
+def digitalloan(request):
+    return render(request, 'digitalloan.html',{'navname':'digitalloan'})
+
+def termscond(request):
+    return render(request, 'tc.html')
+
+def privacy(request):
+    return render(request, 'Customertc.html')
+
+def cardtc(request):
+    return render(request, 'Membershipctc.html')
